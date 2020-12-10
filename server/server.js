4857b7870fd8828f9f0f101638aef9ffb0ffe6c7
Alexander McDonald <alexgarth05@gmail.com>
2020-12-10 22:55:31 +0000

    Get SocketIO to connect to webpage and allow for connections to be recognised
    M	package.json
    M	public/index.html
    M	server/server.js
    M	yarn.lock

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 9035
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('A new user just connected.');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  });
+});
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.listen(port, () => console.log(`Server is online at http://localhost:${port}/`))
+server.listen(port, () => console.log(`Server is online at http://localhost:${port}/`))
